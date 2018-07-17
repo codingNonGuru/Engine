e222745709ce93d4efaed30474fcbd9e7b0968e6
Unknown <andreialbu50@yahoo.com>
2018-07-17 16:54:12 +0300

    Update SceneBuilder

--- Source/Game/SceneBuilder.cpp
+++ Source/Game/SceneBuilder.cpp
@@ -1,10 +1,11 @@
 #include "SceneManager.hpp"
 
-#include "SceneBuilder.hpp"
-#include "Game/MainScene.hpp"
+#include "Game/SceneBuilder.hpp"
+#include "Game/WorldScene.hpp"
+#include "Game/Types.hpp"
 
 void SceneBuilder::GenerateScenes()
 {
-	SceneManager::AddScene(new MainScene(), "MainScene");
+	SceneManager::AddScene(new WorldScene(), Scenes::WORLD);
 }
 
