4a727382fa4f59303ddbcbb86ce14973c20f2553
Unknown <andreialbu50@yahoo.com>
2018-07-20 15:35:22 +0300

    Add WorldScene Enable delay to State

--- Source/Game/States/WorldState.cpp
+++ Source/Game/States/WorldState.cpp
@@ -1,9 +1,32 @@
+#include "Scene.hpp"
+#include "SceneManager.hpp"
+#include "TaskManager.hpp"
+
 #include "Game/States/WorldState.hpp"
+#include "Game/Types.hpp"
+
+WorldState::WorldState()
+{
+	worldScene_ = SceneManager::Get(Scenes::WORLD);
+}
 
 void WorldState::HandleEnter()
 {
+	TaskManager::Add()->Initialize(this, &WorldState::HandleSceneEnable, 0.5f);
 }
 
 void WorldState::HandleExit()
 {
+	if(worldScene_ != nullptr)
+	{
+		worldScene_->Disable();
+	}
+}
+
+void WorldState::HandleSceneEnable()
+{
+	if(worldScene_ != nullptr)
+	{
+		worldScene_->Enable();
+	}
 }
--- Source/Game/States/WorldState.hpp
+++ Source/Game/States/WorldState.hpp
@@ -2,10 +2,20 @@
 
 #include "State.hpp"
 
+class Scene;
+
 class WorldState : public State
 {
+private:
+	Scene* worldScene_;
+
+	void HandleSceneEnable();
+
 protected:
 	virtual void HandleEnter() override;
 
 	virtual void HandleExit() override;
+
+public:
+	WorldState();
 };
